ea364fc813edd9fd0282baa2fd9f684473ce83fb
Ivo Brandao <ivo.couceiro.brandao@gmail.com>
2023-09-29 20:35:36 +0200

    update tests

--- app/source/app_main.cpp
+++ app/source/app_main.cpp
@@ -1,1 +1,9 @@
 #include "app_main.h"
+#include "Systick.h"
+
+void app_main() {
+
+  Systick systick(0, 1000000, 1000);
+  systick.Enable();
+  systick.Disable();
+}
--- app/source/main.cpp
+++ app/source/main.cpp
@@ -1,7 +1,8 @@
+#include "app_main.h"
 #include <iostream>
 #include <string>
 
 int main(int argc, char **argv) {
-  std::cout << "Hello, world!" << std::endl;
+  app_main();
   return 0;
 }
